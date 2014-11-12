aaee0a63e3226f046e97b8a7cd988fd2bd4fa9e6
NathanChristopher <christon@onid.oregonstate.edu>
2014-11-11 16:48:56 -0800

    Added multipathQueueHandler function

--- MultipathDetector.py
+++ MultipathDetector.py
@@ -7,6 +7,18 @@ class MultipathDetector():
     xmlFilePath = "ui.xml"
 
     @staticmethod
+    def parseTupleFile():
+        filePath = "cartesianTupleFile.txt"
+        fileObject = open(filePath, 'r')
+
+        fileObject.seek(2, 0)
+
+
+        fileObject.close()
+
+
+
+    @staticmethod
     def computeDistance(coord1, coord2):
         coord1_x, coord1_y = coord1
         coord2_x, coord2_y = coord2
@@ -95,6 +107,54 @@ class MultipathDetector():
             return True
 
         return False
+
+    # This will take a queue of sets of tuples, where each set of tuples is one tuple from each receiver
+    # Then, we will compute 3 tuples that are the averaged x- and y-values of each tuple from a given receiver
+    # Finally, we will use those 3 averaged tuples to call multipathDetect
+
+    # The call to multipathQueueHandler must guarantee that the queues are fully populated
+    # queue2 will be the center receiver
+    @staticmethod
+    def multipathQueueHandler(listOfQueues):
+        if len(listOfQueues) != 3:
+            print "ERROR: List of queues contains an incorrect number of queues (exactly 3 needed)"
+
+        queue1 = listOfQueues[0]
+        queue2 = listOfQueues[1]
+        queue3 = listOfQueues[2]
+
+        if len(queue1) != len(queue2) or len(queue1) != len(queue3) or len(queue2) != len(queue3):
+            print "ERROR: Queues are not of equal length"
+
+        if len(queue1) != 10:
+            print "ERROR: Queues are not of length 10"
+
+        xCoordAvg_1 = 0
+        xCoordAvg_2 = 0
+        xCoordAvg_3 = 0
+        yCoordAvg_1 = 0
+        yCoordAvg_2 = 0
+        yCoordAvg_3 = 0
+
+
+        for i in queue1:
+            xCoord1, yCoord1 = queue1[i]
+            xCoord2, yCoord2 = queue2[i]
+            xCoord3, yCoord3 = queue3[i]
+
+            xCoordAvg_1 = xCoordAvg_1 + xCoord1
+            xCoordAvg_2 = xCoordAvg_2 + xCoord2
+            xCoordAvg_3 = xCoordAvg_3 + xCoord3
+
+            yCoordAvg_1 = yCoordAvg_1 + yCoord1
+            yCoordAvg_2 = yCoordAvg_2 + yCoord2
+            yCoordAvg_3 = yCoordAvg_3 + yCoord3
+
+        xCoordAvg_1 = xCoordAvg_1 / len(queue1)
+        xCoordAvg_2 = xCoordAvg_2 / len(queue2)
+        xCoordAvg_3 = xCoordAvg_3 / len(queue3)
+
+        return MultipathDetector.multipathDetect((xCoordAvg_1, yCoordAvg_1), (xCoordAvg_2, yCoordAvg_2), (xCoordAvg_3, yCoordAvg_3))
 
 
 
