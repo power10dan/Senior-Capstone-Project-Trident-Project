66eaf9b7db2e42efc623b484bc914ad73f3638bd
power10dan <lintzu@onid.oregonstate.edu>
2015-02-12 06:47:01 -0800

    TextInput And Changeable Panel On GUI Complete

--- trident_GUI/GUI.py
+++ trident_GUI/GUI.py
@@ -1,31 +1,63 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
-from kivy.properties import BooleanProperty, ListProperty
+from kivy.properties import NumericProperty, BooleanProperty, ListProperty
 from os.path import dirname, join
 
-class Data_Showcase(Screen):
+class DataShowcase(Screen):
     fullscreen = BooleanProperty(False)
 
     def add_widget(self, *args):
         if 'content' in self.ids:
-            return self.aids.content.add_widget(*args)
-        return super(Data_Showcase, self).add_widget(*args)
+            return self.ids.content.add_widget(*args)
+        return super(DataShowcase, self).add_widget(*args)
 
-class Trident_LayoutApp(App):
+class TridentLayoutApp(App):
+    index = NumericProperty(-1)
     screen_names = ListProperty([])
     hierarchy = ListProperty([])
 
     def build(self):
         self.buildtitle = "Hello World"
-        self.screen = {}
-        self.available_screens = ['Data View']
+        self.screens = {}
+        self.available_screens = ['TextInputs','DataCarousel']
         self.screen_names = self.available_screens
-        curdir = dirname(__file__)
-        self.available_screens = [join(curdir,'{}.kv'.format(fn)) for fn in self.available_screens]
+        cur_dir = dirname(__file__)
+        self.available_screens = [join(cur_dir, '{}.kv'.format(fn)) for fn in self.available_screens]
+        self.go_next_screen()
 
     def go_previous_screen(self):
         self.index = (self.index - 1) % len(self.available_screens)
         screen = self.load_screen(self.index)
+        sm = self.root.ids.sm
+        sm.switch_to(screen, direction='left')
 
-Trident_LayoutApp().run()
+    def go_next_screen(self):
+        self.index = (self.index +1) % len(self.available_screens)
+        screen = self.load_screen(self.index)
+        sm = self.root.ids.sm
+        sm.switch_to(screen, direction='right')
+
+    def go_screen(self, idx):
+        self.index = idx
+        self.root.ids.sm.switch_to(self.load_screen(idx), direction='left')
+
+    def go_hierarchy_previous(self):
+        ahr = self.hierarchy
+        if len(ahr) == 1:
+            return
+        if ahr:
+            ahr.pop()
+        if ahr:
+            idx = ahr.pop()
+            self.go_screen(idx)
+
+    def  load_screen(self, index):
+        if index in self.screens:
+            return self.screens[index]
+        screen = Builder.load_file(self.available_screens[index].lower())
+        self.screens[index] = screen
+        return screen
+
+
+TridentLayoutApp().run()
