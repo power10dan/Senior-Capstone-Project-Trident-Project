b1fb6d88a340b0b1b8192b4480707ec382c2959f
Nathan Christopher <nathanc@lucidyne.com>
2015-03-03 10:29:56 -0800

    updated GUI code to conform to aesthetic coding standards

--- trident_GUI/GUI.py
+++ trident_GUI/GUI.py
@@ -11,6 +11,8 @@ from kivy.properties import ObjectProperty, OptionProperty
 from kivy.uix.label import Label
 from kivy.adapters.simplelistadapter import SimpleListAdapter
 from kivy.uix.listview import ListView
+
+
 class DataShowcase(Screen):
     fullscreen = BooleanProperty(False)
 
@@ -19,19 +21,23 @@ class DataShowcase(Screen):
             return self.ids.content.add_widget(*args)
         return super(DataShowcase, self).add_widget(*args)
 
+
+
 class TridentLayoutApp(App):
     index = NumericProperty(-1)
     screen_names = ListProperty([])
     hierarchy = ListProperty([])
 
+
     def build(self):
         self.buildtitle = "Poseidon Build"
         self.screens = {}
-        self.available_screens = ['TextInputs','DataCarousel']
+        self.available_screens = ['TextInputs', 'DataCarousel']
         self.screen_names = self.available_screens
         cur_dir = dirname(__file__)
         self.available_screens = [join(cur_dir, '{}.kv'.format(fn)) for fn in self.available_screens]
         self.go_next_screen()
+
 
     def go_previous_screen(self):
         self.index = (self.index - 1) % len(self.available_screens)
@@ -39,19 +45,25 @@ class TridentLayoutApp(App):
         sm = self.root.ids.sm
         sm.switch_to(screen, direction='right')
 
+
     def go_next_screen(self):
-        self.index = (self.index +1) % len(self.available_screens)
+        self.index = (self.index + 1) % len(self.available_screens)
         screen = self.load_screen(self.index)
         sm = self.root.ids.sm
         sm.switch_to(screen, direction='left')
+
+
     def popup_open(self):
-        list_simple_adapter = SimpleListAdapter(data = ["Router 1", "Router 2", "Router 3"], cls=Label)
+        list_simple_adapter = SimpleListAdapter(data=["Router 1", "Router 2", "Router 3"], cls=Label)
         list_view = ListView(adapter = list_simple_adapter)
         popup = Popup(title="Router Selections", content=list_view,size_hint = (None, None), size=(250,250))
         popup.open()
+
+
     def go_screen(self, idx):
         self.index = idx
         self.root.ids.sm.switch_to(self.load_screen(idx), direction='left')
+
 
     def go_hierarchy_previous(self):
         ahr = self.hierarchy
@@ -63,24 +75,28 @@ class TridentLayoutApp(App):
             idx = ahr.pop()
             self.go_screen(idx)
 
+
     def load_screen(self, index):
         if index in self.screens:
             return self.screens[index]
         screen = Builder.load_file(self.available_screens[index].lower())
         self.screens[index] = screen
         return screen
+
+
     def display_settings(self, settings):
-        p = self.settings_popup;
+        p = self.settings_popup
         if p is None:
-            self.settings_popup = p = Popup(content=settings,
-                                            title= 'Settings',
-                                            size_hint=(0.8, 0.8))
+            self.settings_popup = p = Popup(content=settings, title='Settings', size_hint=(0.8, 0.8))
         if p.content is not settings:
             p.content = settings
         p.open()
 
+
     def set_settings_cls(self, panel_type):
         self.settings_cls = panel_type
+
+
     def set_display_type(self, display_type):
         self.destroy_settings()
         self.display_type = display_type
