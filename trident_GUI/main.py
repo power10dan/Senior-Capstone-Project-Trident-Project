d0d0c9ed35e78ec030694b1b9df07f218d592a3e
le90 <albertle5@gmail.com>
2015-04-09 18:38:22 -0700

    got dropdowns working

--- trident_GUI/main.py
+++ trident_GUI/main.py
@@ -6,6 +6,7 @@ from kivy.uix.listview import ListView
 from kivy.uix.dropdown import DropDown
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 import logging
 import xml.etree.ElementTree as ET
 import re
@@ -16,49 +17,64 @@ logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG, format='%(asctim
 
 xmlFilePath = '..//ui.xml'
      
-class SettingsMenu(Widget):
+class SettingsMenu(GridLayout):
     tree = ET.parse(xmlFilePath)
-
+    leftReceiver = ObjectProperty(None)
+    centerReceiver = ObjectProperty(None)
+    rightReceiver = ObjectProperty(None)
+    
     vertical_tolerance = NumericProperty(float(list(tree.iter('vertical'))[0].text))
     horizontal_tolerance = NumericProperty(float(list(tree.iter('horizontal'))[0].text))
     gps_spacing = NumericProperty(float(list(tree.iter('gps_spacing'))[0].text))
     antennaHeight = NumericProperty(float(list(tree.iter('antennaHeight'))[0].text))
     phaseCenter = NumericProperty(float(list(tree.iter('phaseCenter'))[0].text))
     
-    def dropdown(self,loc):
+    def dropdown(self,button):
         receiverList = []
         tree = ET.parse(xmlFilePath)
         RSearch = tree.iter('receiver')
-        
         for r in RSearch:
             if (list(r)[0].text).upper() == 'T':
                 receiverList.append(str(list(r)[4].text))
+        
         drop = DropDown()
         for receiver in receiverList:
             btn = Button(text='%s' % receiver, size_hint_y=None, height=44)
-            btn.bind(on_release=lambda btn: drop.select(btn.text))
+            btn.bind(on_press=lambda btn: drop.select(btn.text))
             drop.add_widget(btn)
-        mainButton = Button(text='Hello', size_hint=(None, None))
-        self.ids[loc].bind(on_release=drop.open)
-        drop.bind(on_select=lambda instance, x: setattr(mainButton, 'text', x))
+        button.bind(on_release=drop.open)
+        drop.bind(on_select=lambda instance, x: setattr(button, 'text', x))
         
-    def submit(self,horizontal,vertical,gps_spacing):
+    def submit(self,horizontal,vertical,gps_spacing,antennaHeight,phaseCenter):
         tree = ET.parse(xmlFilePath)
         horizontal = float(re.sub('[^\.0-9]','',horizontal))
         vertical = float(re.sub('[^\.0-9]','',vertical))
         gps_spacing = float(re.sub('[^\.0-9]','',gps_spacing))
+        antennaHeight = float(re.sub('[^\.0-9]','',antennaHeight))
+        phaseCenter = float(re.sub('[^\.0-9]','',phaseCenter))
         
+        updateTolerance = False
         if self.verifyToleranceValues(horizontal,vertical,gps_spacing):
-            if vertical != SettingsMenu.vertical_tolerance:
+            if vertical != self.vertical_tolerance:
+                updateTolerance = True
                 list(tree.iter('vertical'))[0].text = str(vertical)
-            if horizontal != SettingsMenu.horizontal_tolerance:
+            if horizontal != self.horizontal_tolerance:
+                updateTolerance = True
                 list(tree.iter('horizontal'))[0].text = str(horizontal)
-            if gps_spacing != SettingsMenu.gps_spacing:
+            if gps_spacing != self.gps_spacing:
+                updateTolerance = True
                 list(tree.iter('gps_spacing'))[0].text = str(gps_spacing)
-            if horizontal != SettingsMenu.horizontal_tolerance or \
-               vertical != SettingsMenu.vertical_tolerance or \
-               gps_spacing != SettingsMenu.gps_spacing:
+            if antennaHeight != self.antennaHeight:
+                updateTolerance = True
+                list(tree.iter('gps_spacing'))[0].text = str(gps_spacing)
+            if phaseCenter != self.phaseCenter:
+                updateTolerance = True
+                list(tree.iter('gps_spacing'))[0].text = str(gps_spacing)
+            if updateTolerance:
                 tree.write(xmlFilePath)
+                self.vertical_tolerance = vertical
+                self.horizontal_tolerance = horizontal
+                self.gps_spacing = gps_spacing
 
     # Checks tolerance inputs to ensure they are within the allowed range
     # Returns 'False' if values are unacceptable, 'True' otherwise, and creates a warning in GUI_log.log
@@ -78,7 +94,8 @@ class SettingsMenu(Widget):
 
 
 class DataOutput(Widget):
-    pass
+    def Page(screen):
+        DataOutput.navigation.screenName.text = screen
                
 class Poseidon(Widget):
     pass
